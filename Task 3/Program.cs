70388ce0b2124f4ebaaf2a4783014b4ede04f534
Yana <yadivenko@gmail.com>
2023-01-30 21:08:29 +0300

    Task3

---
+++ Task 3/Program.cs
@@ -0,0 +1,13 @@
+﻿int [] arr = new int [8];
+Random Rnd = new Random ();
+
+
+for (int i = 0; i < arr.Length; i++)
+{
+    arr [i] = Rnd.Next (100);
+}
+
+for (int i = 0; i < arr.Length; i++)
+{
+    System.Console.Write(arr [i] + " ");
+}
