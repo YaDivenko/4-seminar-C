f2e02bdef3c2caed66c0960240fe66059b81b985
Yana <yadivenko@gmail.com>
2023-01-30 21:07:50 +0300

    Task2

---
+++ Task 2/Program.cs
@@ -0,0 +1,11 @@
+﻿System.Console.Write("Введите число: ");
+int a = Convert.ToInt32 (Console.ReadLine ());
+int sum=0;
+
+while (a > 0)
+{
+    sum += a % 10;
+    a /=10;
+}
+
+System.Console.WriteLine(sum);
