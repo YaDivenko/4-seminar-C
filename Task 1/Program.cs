b2412a8d3a5642cae9ec039b9308e8814794a965
Yana <yadivenko@gmail.com>
2023-01-30 21:04:34 +0300

    Task1

---
+++ Task 1/Program.cs
@@ -0,0 +1,14 @@
+﻿System.Console.Write("Введите число A: ");
+int A = Convert.ToInt32 (Console.ReadLine ());
+
+System.Console.Write("Введите степень B: ");
+int B = Convert.ToInt32 (Console.ReadLine ());
+
+int myPow(int a, int b) 
+{
+    int Pow = 1;
+    for (int  i=0; i<b; i++) Pow *= a;
+    return Pow;
+}
+
+System.Console.WriteLine(myPow(A,B));
